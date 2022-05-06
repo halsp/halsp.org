605f6a859995f09b0c61d36b5ee2e4d6f89ed30d
hal-wang <hi@hal.wang>
2022-05-06 10:46:37 +0800

    chore: change publish repo

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 set -e
 
 cd docs/.vuepress/dist
-git init -b main
+git init -b gh-pages
 git add .
 git commit -m "publish"
-git remote add origin git@github.com:sfajs/website-dist.git
-git push origin main -f
+git remote add origin git@github.com:sfajs/sfajs.com.git
+git push origin gh-pages -f
